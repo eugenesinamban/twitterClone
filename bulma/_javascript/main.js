c5a0bcd9ed85250d87aec33958c9df353d589fac
eugenesinamban <49954579+eugenesinamban@users.noreply.github.com>
2020-03-18 18:18:23 +0900

    added jquery

--- bulma/_javascript/main.js
+++ bulma/_javascript/main.js
@@ -2,33 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
 
-var login = document.getElementById('login');
-var signUp = document.getElementById('signUp');
-var modalLogin = document.getElementById('modal-login');
-var modalSignUp = document.getElementById('modal-signUp');
-var modalBackground0 = document.getElementsByClassName('modal-background')[0];
-var modalBackground1 = document.getElementsByClassName('modal-background')[1];
+$('#login').click(() => {
+  $('#modal-login').addClass('is-active');
+});
 
-login.onclick = function() {
-  console.log('test');
-  modalLogin.classList.add('is-active');
-}
+$('#signUp').click(() => {
+  $('#modal-signUp').addClass('is-active');
+});
 
-modalBackground0.onclick = function() {
-  console.log('test');
-  modalLogin.classList.remove('is-active');
-}
+$('.modal-background').click(() => {
+  $('#modal-login').removeClass('is-active');
+  $('#modal-signUp').removeClass('is-active');
+});
 
-signUp.onclick = function() {
-  console.log('test');
-  modalSignUp.classList.add('is-active');
-}
-
-modalBackground1.onclick = function() {
-  console.log('test');
-  modalSignUp.classList.remove('is-active');
-}
-
-// window.onclick = function(event) {
-//   this.console.log(event);
-// }
